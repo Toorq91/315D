bf3260b9c5c5a2d017b9e62144e489238fc53adc
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-05-24 14:37:32 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿public class Ord
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("Please write some words here: ");
+            string input = Console.ReadLine();
+            string wordLength = WordLength(input);
+            int counts = wordLength.Count();
+            string[] words = input.Split(' ');
+            int wordCounter = words.Count();
+            int longestWord = LongestWordLength(input);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"You typed:");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{counts} chars");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{wordCounter} words");
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"Biggest word is {longestWord} chars. \n");
+
+        }
+    }
+
+    static string WordLength(string word)
+    {
+        char[] charArray = word.ToCharArray();
+        return new string(charArray);
+    }
+
+    static int LongestWordLength(string input)
+    {
+        string[] words = input.Split(' ');
+        var maxLength = 0;
+
+        foreach (var word in words)
+        {
+            if (word.Length > maxLength)
+            {
+                maxLength = word.Length;
+            }
+        }
+        return maxLength;
+    }
+}
